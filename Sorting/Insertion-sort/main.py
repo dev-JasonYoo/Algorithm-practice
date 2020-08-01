dc5b0a0defe946bebca5a57bd405a4dc4376e13d
dev-JasonYoo <jasonshyoo@gmail.com>
2020-08-01 22:00:59 +0900

    IMPROVED: more efficient algorithm

--- Sorting/Insertion-sort/main.py
+++ Sorting/Insertion-sort/main.py
@@ -1,21 +1,14 @@
-# !/usr/bin/env python
-
-# Insertion sort algorithm
-# Since made without any guide, this algorithm may not be the most efficient for insertion sort
+from random import shuffle
 
 def insertsort(iterable) :
-    
-    for current in range(len(iterable)) : # 'current' is the index of the element being sorted
+    for current in range(1,len(iterable)) : # iterable[0] is consdiered as resorted in the beginning
+        current_value = iterable[current] # saving current value
+        compare = current
+        while 0 < compare and iterable[compare-1] > current_value : # finding out where to put the sorted current value
+            iterable[compare] = iterable[compare-1] # this line in the while loop moves the elements not sorted
+            compare -= 1 
 
-        compare, n = 0,0 # Initializes 'compare' and 'n' which is used in for-loop in each current for-loop
-        current_value = iterable[current] # Saves the element being sorted since the corresponding space will be assigned by the leading value
-        
-        for compare in range(0,current) : # Compares the element from [0] to the element right ahead of [current]            
-            if not iterable[compare] < iterable[current] : # Finds where to insert 'current_value,' stored value of [current]
+        iterable[compare] = current_value # put current value into the right place that the while loop found out
 
-                for n in range(current,compare,-1) : # Translate the values to one space to make the space where 'current_value' will be put empty
-                    iterable[n] = iterable[n-1]
-                    
-                iterable[compare] = current_value # Insert the saved value of [current] to the right place
-                
+
     return(iterable)
