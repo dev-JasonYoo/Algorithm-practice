f286ea4795cb36862f2985af1e4e076eb9dd2272
dev-JasonYoo <jasonshyoo@gmail.com>
2020-08-05 13:53:48 +0900

    shorter version, attached shebang

--- Sorting/Bubble-sort/main.py
+++ Sorting/Bubble-sort/main.py
@@ -1,9 +1,12 @@
+# !/user/bin/env python3
+
+# Bubble sort algorithm
+
 def bubblesort(iterable) :
-    while True: # Infinite loop untill sorted
-        count = 1 # If for-loop is not executed, count remains 1, which means that it's all sorted and leads to break.
+    yet = True
+    while yet: # Infinite loop untill sorted ( = untill 'yet == True')
+        yet = False # If 'yet' remains 'False', it means they're all sorted
         for index in range(1,len(iterable)) :
             if iterable[index-1] > iterable[index] : # if an element of a lower index is bigger,
                 iterable[index-1], iterable[index] = iterable[index], iterable[index-1] # swap them
-                count *= 0 # 'count == 0' means 'not sorted yet'
-        if count == 1 : # 'count == 1' means 'all sorted'
-            break
+                yet == True # If 'yet' is 'True', it means they are not yet sorted
