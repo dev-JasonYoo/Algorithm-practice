98e90e03fa4891e4964f020ca81656545a208439
dev-JasonYoo <jasonshyoo@gmail.com>
2021-07-04 07:58:26 +0000

    Shorten the solution code of no.1934

--- Math/1934_LCM.py
+++ Math/1934_LCM.py
@@ -1,26 +1,21 @@
 #!/usr/bin/env python
 #No.1934
 #Memory: 29452KB
-#Time: 112ms
-#Length: 370B
+#Time: 160ms
+#Length: 272B
 
 
 def euc_lcm(a,b): #Returns LCM of 'a' and 'b' by Euclidean Algorithm
-    if a<b:
-        a,b = b,a
     if a%b == 0 :
         return b
-    a = a%b
-    return euc_lcm(a,b)
+    return euc_lcm(b,a%b)
 
 def gcd(a,b):
     return int(a*b/euc_lcm(a,b))
 
 t = int(input())
-num = []
 
-for i in range(t):
-    num.append(list(map(int,input().split(" "))))
-
-for i in range(t):
-    print(gcd(num[i][0],num[i][1]))
+while t:
+    a,b=map(int,input().split(" "))
+    print(gcd(a,b))
+    t -= 1
