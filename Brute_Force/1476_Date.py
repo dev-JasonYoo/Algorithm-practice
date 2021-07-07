0b96a185e5934f1ee3bdd24bbb79694872a27678
dev-JasonYoo <jasonshyoo@gmail.com>
2021-07-07 14:46:27 +0000

    Edited no.1476 toward faster algorithm

--- Brute_Force/1476_Date.py
+++ Brute_Force/1476_Date.py
@@ -1,22 +1,11 @@
-esm = list(map(int,input().split()))
-count = [1,1,1]
+e,s,m = map(int,input().split())
 y = 1
 
 while True:
-    if count==esm:
+    if (y-e)%15==0 and (y-s)%28==0 and (y-m)%19==0: 
+        #Instead of using x%y==r, use (x-r)%y==0 that is a lot faster while they conduct the same calculation.
+        #Division operator, including modulo opeartor, should involve a number as small as possible since its time complexity isn't that efficient
+        #https://stackoverflow.com/questions/27977834/why-is-modulus-operator-slow
         print(y)
         break
-    
     y+=1
-    for i in range(3):
-        count[i]+=1
-    
-    if count[0]==16:
-        count[0] = 1
-        
-    if count[1]==29:
-        count[1] = 1
-        
-    if count[2]==20:
-        count[2] = 1
-    
