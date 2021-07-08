dfc34ff6d31a9ee16edd080a429d4c6dfd5567d8
dev-JasonYoo <jasonshyoo@gmail.com>
2021-07-08 14:49:27 +0000

    Slow version of no.7849. Not pass yet

---
+++ Math/7894_BigFactorial.py
@@ -0,0 +1,22 @@
+import sys
+
+sys.setrecursionlimit(10**7)
+
+t = int(input())
+
+for _ in range(t):
+    x = int(input())
+    fac = 1
+    def factorial(n): #recursive function finding factorial
+        global fac
+        if n<=1:
+            return fac
+        fac*=n
+        n-=1
+        return factorial(n)
+
+    def factorial_len(n): #function finding the number of digits of factorial of n.
+        li = list(str(factorial(n))) #ex) list(str(120)): [1,2,0]
+        print(len(li))
+    
+    factorial_len(x)
